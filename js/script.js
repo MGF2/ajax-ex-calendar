0dd02039ff9e5a2d1683657e3ff3ee8791499cc6
Unknown <fioremartina@hotmail.com>
2020-08-31 15:55:37 +0200

    holidays functions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
 
+  //data di partenza
   var today = moment('2018-01-01');
-  
+
+  //inserimento giorni del mese
   insertDays(today);
+  //inserimento festività
+  insertHolidays(today);
 
 
 
@@ -17,6 +21,7 @@ function addZero(n) {
     return n;
   }
 }
+
 
 function insertDays(data) {
   var month = data.format('MMMM');
@@ -42,3 +47,83 @@ function insertDays(data) {
     $('.month-list').append(html);
   }
 }
+
+
+function insertHolidays(data) {
+
+  $.ajax(
+    {
+      url: "https://flynn.boolean.careers/exercises/api/holidays",
+      method: "GET",
+      data: {
+        year: data.year(),        //dati richiesti all API
+        month: data.month()
+      },
+      success: function (risposta) {
+
+        console.log(risposta);
+        console.log(data);
+
+        var response = risposta.response;
+
+        //ciclo per inserimento dati API
+        for (i = 0; i < response.length; i++) {
+
+          //Inserimento data API in list item
+          var dataLi = $('li[data-complete-date="' + response[i].date + '"]');
+          console.log(dataLi);
+
+          //Memorizzazione nome festività
+          var nameHoliday = response[i].name;
+
+          //Aggiunta nome festività al documento
+          dataLi.append( ' ' + nameHoliday);
+          console.log(nameHoliday);
+          //Aggiunta colore a festività
+          dataLi.addClass('holidays');
+
+        }
+
+      },
+      error: function (data, stato, errori) {
+        alert("E' avvenuto un errore.");
+      }
+
+    }
+  );
+
+
+
+}
+
+
+
+// RESPONSE API
+// {
+//     "success": true,
+//     "response": [
+//         {
+//             "name": "Capodanno",
+//             "date": "2018-01-01"
+//         },
+//         {
+//             "name": "Epifania",
+//             "date": "2018-01-06"
+//         }
+//     ]
+// }
+
+//AJAX EXMP
+
+// $.ajax(
+// {
+// url: "http://www.boolean.careers/api/random/boolean",
+// method: "GET",
+// success: function (data, stato) {
+// $("#risultati").html(data);
+// },
+// error: function (richiesta, stato, errori) {
+// alert("E' avvenuto un errore. " + errore);
+// }
+// }
+// );
